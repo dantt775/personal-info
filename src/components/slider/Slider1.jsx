b895d1f9da2610d6ad3706a85f9b01fc1341811a
Daniel Marchi Sila <daniel.marchi@outlook.com>
2018-08-08 13:29:54 -0300

    Text

--- src/components/slider/Slider1.jsx
+++ src/components/slider/Slider1.jsx
@@ -8,10 +8,10 @@ export default class Slider1 extends Component {
             <section id='slider1'>
                 <div className='slider-content'>
 
-                <div className='container'>
-                    <h1>DEVELOPER</h1>
-                    <p>ReactJS, NodeJS, JavaScript, HTML, CSS, BootStrap.</p>
-                </div>
+                    <div className='container'>
+                        <h1>DEVELOPER</h1>
+                        <p>ReactJS, NodeJS, JavaScript, HTML, CSS, BootStrap.</p>
+                    </div>
                 </div>
             </section>
         )
