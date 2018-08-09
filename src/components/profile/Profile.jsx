3f101f98b5aa107a4e56a7adb5a4de2aeaa60ece
Daniel Marchi Sila <daniel.marchi@outlook.com>
2018-08-09 16:17:49 -0300

    Texts and some "styles" into profile component

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,10 +1,59 @@
 import React, { Component } from 'react'
 
+import './profile.scss'
+
 export default class Profile extends Component {
     render() {
         return (
             <section id="profile">
-            
+                <div className='container'>
+
+                    <div className='row'>
+
+                        <div className='col-lg-12'>
+                            <h1 className='profile-title'>Perfil</h1>
+
+                            <div className='col-lg-4'>
+                                <h1 className='about-title'> Sobre mim </h1>
+                                <p className='about-p'>
+                                    Lorem ipsum cubilia aliquet nostra dictum, vehicula egestas habitasse nisi curabitur tellus,
+                                    vitae quisque neque tempus. vehicula malesuada est nisl velit ut vel non augue molestie risus,
+                                    torquent ligula aenean conubia hac quisque pulvinar est fermentum magna, eu interdum aliquet
+                                    facilisis suscipit tempor inceptos rutrum ad. arcu nibh blandit nostra semper sed porta lectus
+                                    nam libero, maecenas arcu pharetra nec eget lobortis odio class donec, etiam ullamcorper feugiat
+                                    non ultrices vivamus cursus curabitur. duis neque eros torquent ligula semper lacinia sociosqu hac
+                                    purus tristique rutrum enim feugiat sed, gravida senectus vestibulum tincidunt dictum primis habitasse
+                                    condimentum quisque litora nullam euismod.
+
+                                </p>
+                            </div>
+
+                            <div className='col-lg-4 col-lg-offset-0'>
+                                <div className='avatar-container'>
+                                    <img className='avatar' src={require('../../assets/images/avatar.png')} />
+                                </div>
+                            </div>
+
+                            <div className='col-lg-4'>
+                                <h1 className='details-title'> Detalhes </h1>
+
+                                <h2 className='details-subtitle'> Nome </h2>
+                                <p className='details-p'> Daniel Marchi Silva </p>
+
+                                <h2 className='details-subtitle'> Idade </h2>
+                                <p className='details-p'> 25 anos </p>
+
+                                <h2 className='details-subtitle'> Local </h2>
+                                <p className='details-p'> São Paulo / SP </p>
+
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
 
 
 
--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import ReactDOM from 'react-dom'
 import Header from './components/header/Header.jsx'
 import Footer from './components/footer/Footer.jsx'
 import Carousel from './components/carousel/Carousel.jsx'
-import Slider1 from './components/slider/Slider1.jsx'
-import Slider2 from './components/slider/Slider2.jsx'
-import Slider3 from './components/slider/Slider3.jsx'
+import Profile from './components/profile/Profile.jsx'
 import "./assets/scss/main.scss"
 import '../node_modules/slick-carousel/slick/slick.css'
 import '../node_modules/slick-carousel/slick/slick-theme.css';
@@ -18,7 +16,9 @@ import '../node_modules/slick-carousel/slick/slick-theme.css';
 
 ReactDOM.render(
     <div>    
+    <Header />
     <Carousel />
+    <Profile />
     <Footer />
     </div>
     , document.getElementById('app'))
