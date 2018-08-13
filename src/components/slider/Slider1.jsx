9e294c4f650cda8b53669cc5e0ceee477f471710
Daniel Marchi Sila <daniel.marchi@outlook.com>
2018-08-13 13:27:48 -0300

    test

--- src/components/slider/Slider1.jsx
+++ src/components/slider/Slider1.jsx
@@ -9,7 +9,7 @@ export default class Slider1 extends Component {
                     <div className='container'>
                         <div className='row'>
                             <div className='col-lg-12clear'>
-                                <h1 className='h1-slider'>Lorem Ipsum Hell Yeah!</h1>
+                                <h1 className='h1-slider'>Lorem Ipsumrffff Hell Yeah!</h1>
                                 <h2 className='h2-slider'> Lorem Ipsumd Hell Yseeah! </h2>
 
                             </div>
