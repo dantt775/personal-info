b60f92dffc9391615563c0d5007080e082c40a78
unknown <cielo@STFALXC037195-W.stefanini.dom>
2018-08-14 15:25:20 -0300

    Fix indentation

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,10 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
-    module: {
 
+
+    module: {
+        
         rules: [
             {
                 test: /\.js$/,
@@ -11,64 +13,46 @@ module.exports = {
                 use: {
                     loader: "babel-loader"
                 }
-            },
-            {
+            }, {
                 test: /\.jsx$/,
                 use: {
                     loader: "babel-loader"
                 }
-            },
-            {
+            }, {
                 test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader",
-                        options: { minimize: true }
-                    }
-                ]
-            },
-            {
+                use: [{
+                    loader: "html-loader",
+                    options: { minimize: true }
+                }]
+            }, {
                 test: /\.css$/,
                 use: [MiniCssExtractPlugin.loader, "css-loader"]
-            },
-            {
+            }, {
                 test: /\.scss$/,
-                use: [
-                    {
-                        loader: "style-loader" // creates style nodes from JS strings
-                    },
-                    {
-                        loader: "css-loader" // translates CSS into CommonJS
-                    },
-                    {
-                        loader: "sass-loader" // compiles Sass to CSS
-                    }
-                ]
-            },
-            {
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "sass-loader" // compiles Sass to CSS
+                }]
+            }, {
                 test: /\.(png|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {}
-                    }
-                ]
+                use: [{
+                    loader: 'file-loader',
+                    options: {}
+                }]
             }, {
                 test: /\.(svg)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {}
-                    }
-
-                ]
+                use: [{
+                    loader: 'file-loader',
+                    options: {}
+                }]
             }, {
                 test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
                 use: [{
-
                     loader: "file-loader"
-                }
-                ]
+                }]
             }
         ]
     },
