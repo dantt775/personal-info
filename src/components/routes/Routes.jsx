c84d4b2ec3a00335ba193a5c3ab7db875cd73aa2
unknown <cielo@STFALXC037195-W.stefanini.dom>
2018-08-14 15:24:41 -0300

    Added routes files and changed the index.js to render the routes itself

---
+++ src/components/routes/Routes.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { BrowserRouter, Route, Switch, Redirect, browserHistory  } from 'react-router-dom'
+import Home from '../home/Home.jsx'
+
+const Routes = () => (
+    <BrowserRouter history ={browserHistory} >
+        <Switch>
+            <Route path='/home' component={Home} />
+            <Redirect from='*' to="/home" />
+        </Switch>
+    </BrowserRouter>
+)
+export default Routes;
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,29 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { HashRouter, Route, Redirect, BrowserRouter, Switch, browserHistory } from 'react-router-dom'
 
-
-
-import Home from './components/home/Home.jsx'
+import Routes from './components/routes/Routes.jsx'
 import "./assets/scss/main.scss"
 
-
-
-
-
-
-
-
-ReactDOM.render((
-
-    <HashRouter history={browserHistory}>
-        <Switch>
-            <Route path="/home" component={Home} />
-            
-            <Redirect from="*" to="/home" />
-        </Switch>
-    </HashRouter>
-
-),
-    document.getElementById('app')
+ReactDOM.render(
+    <Routes />
+    , document.getElementById('app')
 );
+
+
